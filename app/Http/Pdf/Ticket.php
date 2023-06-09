38cb6a82e7aca6c7455746d25f5b419fb51b43d8
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-06-08 20:12:16 -0400

    fixed

--- app/Http/Pdf/Ticket.php
+++ app/Http/Pdf/Ticket.php
@@ -43,61 +43,62 @@ class Ticket extends Fpdf
         $fecha = date('d/m/Y', strtotime($pedido->fecha));
         $fechaLiteral = $this->fechaLiteral($fecha);
         $this->fpdf->AddPage();
-        $this->fpdf->SetMargins(5, $this->margin, 5);
-        $this->fpdf->SetAutoPageBreak(true, 20);
+        $this->fpdf->SetMargins(5, 0, 5);
+        $this->fpdf->SetAutoPageBreak(true, 10);
         // diseño del ticket donde se muestre el nombre de la empresa la fecha y el numero de pedido tambien los productos
-        $this->fpdf->SetFont('Arial', 'B', 7);
+        $this->fpdf->SetFont('Arial', 'B', 12);
         // $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("CHEESE HOUSE         "), 0, 'C', 0);
         // imagen con el logo
-        $this->fpdf->Image('logo.png', 30, 8, 20, 25, 'PNG');
-        $this->fpdf->Ln(25);
+        $this->fpdf->Image('logo.png', 30, 8, 20, 20, 'PNG');
+        $this->fpdf->Ln(20);
 
         // DIRECCION Y NUMERO
-        $this->fpdf->SetFont('Arial', '', 5);
+        $this->fpdf->SetFont('Arial', '', 9);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Av. Paragua esq Los Socoris entre 2do y 3er anillo."), 0, 'C', 0);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Tel. 64881234."), 0, 'C', 0);
-        $this->fpdf->Ln(2);
+        $this->fpdf->Ln(5);
         // UN SEPARADOR
-        $this->fpdf->SetFont('Arial', 'B', 9);
-        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
-        $this->fpdf->SetFont('Arial', 'B', 7);
-        $this->fpdf->Ln(2);
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
+        $this->fpdf->Ln(3);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("RECIBO DE COMPRA"), 0, 'C', 0);
-        $this->fpdf->Ln(2);
-        $this->fpdf->SetFont('Arial', 'B', 9);
-        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
+        $this->fpdf->Ln(3);
+        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
 
-        $this->fpdf->Ln(1);
-        $this->fpdf->SetFont('Arial', '', 5);
+        $this->fpdf->Ln(3);
+        $this->fpdf->SetFont('Arial', '', 9);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Fecha: " . $fechaLiteral), 0, 'L', 0);
+        $this->fpdf->Ln(2);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("No. Pedido: " . $pedido->codigo_seguimiento), 0, 'L', 0);
+        $this->fpdf->Ln(2);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Cliente: " . $pedido->cliente), 0, 'L', 0);
-        $this->fpdf->SetFont('Arial', 'B', 9);
-        $this->fpdf->Ln(1);
-        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
+        $this->fpdf->Ln(2);
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
 
-        $this->fpdf->Ln(1);
-        $this->fpdf->SetFont('Arial', 'B', 6);
+        $this->fpdf->Ln(2);
+        $this->fpdf->SetFont('Arial', 'B', 10);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("DESCRIPCION"), 0, 'L', 0);
-        $this->fpdf->SetFont('Arial', '', 5);
+        $this->fpdf->SetFont('Arial', '', 9);
+        $this->fpdf->Ln(2);
         foreach ($pedido->detalle_pedidos as $detalle_pedido) {
             $this->fpdf->Cell(50, 5, utf8_decode($detalle_pedido->producto->nombre), 0, 0, 'L');
             $this->fpdf->Cell(20, 5, utf8_decode($detalle_pedido->monto_total . " Bs"), 0, 0, 'R');
             $this->fpdf->Ln(5);
         }
-        $this->fpdf->Ln(2);
-        $this->fpdf->SetFont('Arial', 'B', 9);
-        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
-        $this->fpdf->Ln(2);
-        $this->fpdf->SetFont('Arial', '', 7);
+        $this->fpdf->Ln(3);
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
+        $this->fpdf->Ln(3);
+        $this->fpdf->SetFont('Arial', '', 12);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Total: " . $pedido->monto_total . " Bs"), 0, 'R', 0);
-        $this->fpdf->SetFont('Arial', 'B', 9);
-        $this->fpdf->Ln(2);
-        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
+        $this->fpdf->SetFont('Arial', 'B', 12);
+        $this->fpdf->Ln(3);
+        $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("* * * * * * * * * * * * * * * * * * * * * * * *"), 0, 'C', 0);
 
         // AGRADECIMIETNO
         $this->fpdf->Ln(5);
-        $this->fpdf->SetFont('Arial', '', 5);
+        $this->fpdf->SetFont('Arial', '', 9);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Gracias por su compra"), 0, 'C', 0);
         $this->fpdf->MultiCell($this->width, $this->space, utf8_decode("Vuelva pronto"), 0, 'C', 0);
         $this->fpdf->Output("I", "ticket.pdf");
