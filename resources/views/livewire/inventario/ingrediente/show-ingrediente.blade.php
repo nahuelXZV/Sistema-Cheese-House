c0e879ebbcc79710b0b67ae9f217bb3f882882c0
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-06-30 20:49:30 -0400

    fixed show ingrediente

--- resources/views/livewire/inventario/ingrediente/show-ingrediente.blade.php
+++ resources/views/livewire/inventario/ingrediente/show-ingrediente.blade.php
@@ -35,54 +35,47 @@
     <form class="grid grid-cols-2 gap-3">
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-            <input type="text" wire:model.lazy="ingredienteArray.nombre""
+            <input type="text" value="{{ $ingrediente->nombre }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Harina" required readonly>
-            <x-input-error for="ingredienteArray.nombre" />
+                placeholder="Harina" readonly>
         </div>
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Unidad</label>
-            <input type="text" wire:model.lazy="ingredienteArray.unidad""
+            <input type="text" value="{{ $ingrediente->unidad }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="kg" required readonly>
-            <x-input-error for="ingredienteArray.unidad" />
+                placeholder="kg" readonly>
         </div>
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
-            <input type="number" wire:model.lazy="ingredienteArray.stock" min="0"
+            <input type="text" value="{{ $ingrediente->stock }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="0" required readonly>
-            <x-input-error for="ingredienteArray.stock" />
+                placeholder="0" readonly>
         </div>
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
-            <input type="number" wire:model.lazy="ingredienteArray.precio_unidad" min="0" step="0.01"
+            <input type="text" value="{{ $ingrediente->precio_unidad }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="0" required readonly>
-            <x-input-error for="ingredienteArray.precio_unidad" />
+                placeholder="0" readonly>
         </div>
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock minimo</label>
-            <input type="number" wire:model.lazy="ingredienteArray.stock_minimo" min="0"
+            <input type="text" value="{{ $ingrediente->stock_minimo }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="10" required readonly>
-            <x-input-error for="ingredienteArray.stock_minimo" />
+                placeholder="10" readonly>
         </div>
         <div class="mb-2">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock maximo</label>
-            <input type="number" wire:model.lazy="ingredienteArray.stock_maximo" min="0"
+            <input type="text" value="{{ $ingrediente->stock_maximo }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="100" required readonly>
-            <x-input-error for="ingredienteArray.stock_maximo" />
+                placeholder="100" readonly>
         </div>
 
         <div class="mb-2 col-span-2">
             <label for="message"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripcion</label>
-            <textarea id="message" rows="4" wire:model.lazy="ingredienteArray.descripcion" readonly
+            <textarea id="message" rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="......"></textarea>
-            <x-input-error for="ingredienteArray.descripcion" />
+                placeholder="......">{{ $ingrediente->descripcion }}</textarea>
         </div>
     </form>
 
