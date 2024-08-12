5ef382145dd5ec2e3fa62dc5557b0d9d3a32431d
Nahuel Zalazar <80172547+nahuelXZV@users.noreply.github.com>
2024-08-12 14:09:05 -0400

    update(producto): update view

--- resources/views/livewire/inventario/producto/list-producto.blade.php
+++ resources/views/livewire/inventario/producto/list-producto.blade.php
@@ -98,7 +98,7 @@
                                 @break
                                 @case('Mitad')
                                     <span
-                                        class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Pizza</span>
+                                        class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Mitad</span>
                                 @break
 
                                 @case('Bebida')
